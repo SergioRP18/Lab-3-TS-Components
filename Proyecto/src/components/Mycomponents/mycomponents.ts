f80ad180c3f24868a4bcff9d26590c5d04bd38bd
SergioRP18 <sergio.restrepo@u.icesi.edu.co>
2024-09-13 15:30:52 -0500

    Inicia el laboratorio y logra mostrar en pantalla la data

---
+++ Proyecto/src/components/Mycomponents/mycomponents.ts
@@ -0,0 +1,85 @@
+export enum Attributes {
+    'image' = 'image',
+    'name' = 'name',
+    'uid' = 'uid',
+    'age' = 'age',
+    'gender' = 'gender',
+    'area' = 'area',
+    'position' = 'position',
+    'timeInCompany' = 'timeInCompany',
+    'experience' = 'experience'
+
+}
+class AppCard extends HTMLElement {
+    image?: string;
+    name?: string;
+    uid?: number;
+    age?: number;
+    gender?: string;
+    area?: string;
+    position?: string;
+    timeInCompany?: number;
+    experience?: number;
+
+    constructor(){
+        super();
+        this.attachShadow({mode:'open'});
+    }
+    static get observedAttributes(){
+        const attributesWorkers: Record<Attributes, null> = {
+            image: null,
+            name: null,
+            uid: null,
+            age: null,
+            gender: null,
+            area: null,
+            position: null,
+            timeInCompany: null,
+            experience: null,
+        };
+        return Object.keys(attributesWorkers);
+    }
+    attributeChangedCallback(propName:Attributes, oldValue:string | undefined, newValue:string | undefined){
+        switch(propName){
+            case Attributes.uid:
+                    this.uid = newValue ? Number(newValue) : undefined;
+                    break;
+            case Attributes.age:
+                    this.age = newValue ? Number(newValue) : undefined;
+                    break;
+            case Attributes.timeInCompany:
+                    this.timeInCompany = newValue ? Number(newValue) : undefined;
+                    break;
+            case Attributes.experience:
+                    this.experience = newValue ? Number(newValue) : undefined;
+                    break;
+
+            default:
+                this[propName] = newValue;
+                break;
+        }
+        this.render();
+    }
+    connectedCallback(){
+        this.render;
+    }
+    render(){
+        if(this.shadowRoot){
+            this.shadowRoot.innerHTML = `
+            <section>
+                <img src="${this.image}}">
+                <h1>${this.name}</h1>
+                <h3>${this.uid}</h3>
+                <h4>${this.age}</h4>
+                <h3>${this.gender}</h3>
+                <p>${this.area}</p>
+                <p>${this.position}</p>
+                <h5>${this.timeInCompany}</h5>
+                <h5>${this.experience}</h5>
+            </section>
+            `
+        }
+    }
+}
+customElements.define('app-card',AppCard);
+export default AppCard;
---
+++ Proyecto/src/indexAbuelo.ts
@@ -0,0 +1,36 @@
+import './components/indexPadre';
+import { workers } from './data/data';
+import MyWorkers, { Attributes } from './components/Mycomponents/mycomponents';
+
+class AppContainer extends HTMLElement {
+    person: MyWorkers[] = [];
+
+    constructor(){
+        super();
+        this.attachShadow({mode:'open'});
+
+        workers.forEach((user) => {
+            const workerCard = this.ownerDocument.createElement("app-card") as MyWorkers;
+            workerCard.setAttribute(Attributes.image, user.image);
+            workerCard.setAttribute(Attributes.name, user.name);
+            workerCard.setAttribute(Attributes.uid, String(user.id));
+            workerCard.setAttribute(Attributes.age, String(user.age));
+            workerCard.setAttribute(Attributes.gender, user.gender);
+            // workerCard.setAttribute(Attributes.area, user.area);
+            // workerCard.setAttribute(Attributes.timeInCompany, user.timeInCompany);
+            // workerCard.setAttribute(Attributes.experience, String(user.experience));
+            this.person.push(workerCard);
+        });
+    }
+    connectedCallback(){
+        this.render();
+    }
+    render(){
+        if(this.shadowRoot){
+            this.person.forEach((person) => {
+                this.shadowRoot?.appendChild(person);
+            });
+        }
+    }
+};
+customElements.define('app-container', AppContainer);
